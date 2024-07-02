10d067ef19a2b7a70db61ff1c11e6e44715964b1
bboggo <kmt0819@naver.com>
2024-07-02 19:09:24 +0900

    test

---
+++ src/main/java/com/backend/dorun_api/DorunApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.dorun_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorunApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DorunApiApplication.class, args);
+	}
+
+}
